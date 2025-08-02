3b80fadeb04971479aeae10574be5cc8211dd9d7
seopx2 <tj09qls@naver.com>
2025-08-03 00:31:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ from pydantic import BaseModel
 from typing import List
 from recommender import recommend_recipes
 
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 8000))  # Render가 지정한 포트를 가져옴
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
 app = FastAPI()
 
@@ -39,4 +45,5 @@ def get_recommendations(input: IngredientInput):
 @app.get("/result")
 def result_page(request: Request):
     return templates.TemplateResponse("index_results.html", {"request": request})
+
 
